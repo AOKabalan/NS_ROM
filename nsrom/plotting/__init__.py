75ebd6b7b32b4d38f2f7f80a9e9a16a6f7d34c5f
Ali Kabalan <aokabalan@gmail.com>
2026-08-21 19:19:25 +0300

    fix: remove stale plotting style export

--- nsrom/plotting/__init__.py
+++ nsrom/plotting/__init__.py
@@ -1,4 +1,1 @@
-"""Shared plotting utilities for nsrom figures."""
-from .style import set_style, SINGLE_COL, DOUBLE_COL, C_POS, C_NEG, C_SYM
-
-__all__ = ["set_style", "SINGLE_COL", "DOUBLE_COL", "C_POS", "C_NEG", "C_SYM"]
+"""Plotting utilities for NS_ROM."""
